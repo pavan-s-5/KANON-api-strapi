042cb6da37c87ac1b6d1b6d847c90c9fee928abe
Pavan <pavansgowda6@gmail.com>
2023-07-30 15:18:37 +0530

    kanon-api-folder-strapi-stripe-first-commit

---
+++ src/api/termsofuse/routes/termsofuse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * termsofuse router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::termsofuse.termsofuse');
---
+++ src/api/termsofuse/services/termsofuse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * termsofuse service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::termsofuse.termsofuse');
